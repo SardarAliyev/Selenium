8230dcd8e246b3cfaa2cf6c635225bebbedca001
SardarAliyev <eli145798641@gmail.com>
2024-05-23 15:51:51 +0400

    codes

---
+++ sardarrrr.py
@@ -0,0 +1,8 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+service = Service("chromedriver.exe")
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
+driver.get("https://userinyerface.com/game.html")
