bb39b911d6db820d1c051d2fc7600d8e0aa8c6bc
SardarAliyev <eli145798641@gmail.com>
2024-05-24 15:11:50 +0400

    first commit

--- sardarrrr.py
+++ sardarrrr.py
@@ -1,4 +1,5 @@
 import selenium
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -6,3 +7,4 @@ service = Service("chromedriver.exe")
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 driver.get("https://userinyerface.com/game.html")
+time.sleep(10)
